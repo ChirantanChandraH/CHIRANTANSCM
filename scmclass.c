8c78866a6e194720e33b4b989a079829ca0fc71b
Chirantan Chandra H <chirantan.h@s.amity.edu>
2025-02-27 11:41:16 +0530

    commiting the files

---
+++ scmclass.c
@@ -0,0 +1,10 @@
+void 
+int main() {
+	int num1 num2;
+	sum = num1 + num2;
+	printf("enter num1 and num2");
+	scanf("% d %d ", &num1,&num2);
+	printf("sum of two numbers %d",&sum );
+
+	return 0;
+} 
---
+++ subtraction.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main (){
+	int num1 , num2;
+	printf("enter num1 and num2");
+	scanf ("%d %d", &num1,&num2);
+	sum = num1 - num2;
+	printf("sum of num1 and num2 is %d",&sum);
+	return 0;
+} 
